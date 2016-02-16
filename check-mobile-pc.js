bfb2b381a9ab59489438ed6c0b72c7de551dcb05
Colin Davis <nilocsivad@aol.com>
2016-02-16 13:11:21 +0800

    check mobile pc

---
+++ check-mobile-pc.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+document.createElement( 'header' );
+document.createElement( 'footer' );
+document.createElement( 'canvas' );
+document.createElement( 'section' );
+if ( navigator.userAgent.indexOf( "iPhone" ) > 0 || navigator.userAgent.indexOf( "Android" ) > 0 ) {
+	location.href = 'http://m.dcloud.io'
+}
