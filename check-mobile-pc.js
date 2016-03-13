8f8db84e9a786b9e36e0032912e5c7716c45d458
Colin Davis <nilocsivad@aol.com>
2016-03-13 14:14:06 +0800

    c

--- check-mobile-pc.js
+++ check-mobile-pc.js
@@ -7,5 +7,5 @@ document.createElement( 'footer' );
 document.createElement( 'canvas' );
 document.createElement( 'section' );
 if ( navigator.userAgent.indexOf( "iPhone" ) > 0 || navigator.userAgent.indexOf( "Android" ) > 0 ) {
-	location.href = 'http://m.dcloud.io'
+	location.href = 'http://******'
 }
