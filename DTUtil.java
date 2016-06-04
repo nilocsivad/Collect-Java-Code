b27b2c8e8291a83c576c47e4e6d4de1560035d64
Colin <nilocsivad@aol.com>
2016-06-04 16:09:14 +0800

    DTUtil

--- DTUtil.java
+++ DTUtil.java
@@ -5,49 +5,46 @@ import java.util.Date;
  * @author Colin
  */
 public class DTUtil {
-	
-	
-	public static final String	FMT_DEFAULT		= "yyyy-MM-dd HH:mm:ss";
-	public static final String	FMT_DATE		= "yyyy-MM-dd";
-	public static final String	FMT_DATE_HHMM	= "yyyy-MM-dd HH:mm";
-	public static final String	FMT_TIME		= "yyyy-MM-dd";
-	public static final String	FMT_FILE		= "yyyy-MM-dd_HH-mm-ss";
-												
-												
-	/**
-	 * 
-	 */
-	private DTUtil() {}
-	
-	
+
+
+	public static final String FMT_DEFAULT = "yyyy-MM-dd HH:mm:ss";
+	public static final String FMT_DATE = "yyyy-MM-dd";
+	public static final String FMT_DATE_HHMM = "yyyy-MM-dd HH:mm";
+	public static final String FMT_TIME = "HH:mm:ss";
+	public static final String FMT_FILE = "yyyy-MM-dd_HH-mm-ss";
+
+
+	private DTUtil() {
+	}
+
 	public static String nowDT() {
-		
-		return formatDT( FMT_DEFAULT );
+
+		return formatDT(FMT_DEFAULT);
 	}
-	
+
 	public static String nowDate() {
-		
-		return formatDT( FMT_DATE );
+
+		return formatDT(FMT_DATE);
 	}
-	
+
 	public static String nowTime() {
-		
-		return formatDT( FMT_TIME );
+
+		return formatDT(FMT_TIME);
 	}
-	
-	public static String formatDT( String fmt ) {
-		
-		return formatDT( fmt, new Date() );
+
+	public static String formatDT(String fmt) {
+
+		return formatDT(fmt, new Date());
 	}
-	
-	public static String formatDT( String fmt, Date date ) {
-		
-		return getFmt( fmt ).format( date );
+
+	public static String formatDT(String fmt, Date date) {
+
+		return getFmt(fmt).format(date);
 	}
-	
-	public static DateFormat getFmt( String fmt ) {
-		
-		return new SimpleDateFormat( fmt );
+
+	public static DateFormat getFmt(String fmt) {
+
+		return new SimpleDateFormat(fmt);
 	}
-	
+
 }
